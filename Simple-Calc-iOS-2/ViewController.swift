33c604265a57c59a8955148225e43f7a37e60a4e
Vicky Juan <vicky37@uw.edu>
2017-04-24 19:51:32 -0700

    Initial Commit

---
+++ Simple-Calc-iOS-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple-Calc-iOS-2
+//
+//  Created by Vicky Juan on 4/24/17.
+//  Copyright © 2017 Victoria Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
